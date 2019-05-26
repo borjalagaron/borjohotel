437d643fee6009280ee255e4d159f8f34cc07ba7
Borja Lagaron <borjo@Borjas-MacBook-Air.local>
2019-05-26 20:53:25 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { AccommodationComponent } from './accommodation/accommodation.component';
+import { FoodBeverageComponent } from './food-beverage/food-beverage.component';
+import { RestaurantComponent } from './food-beverage/restaurant/restaurant.component';
+import { BarsComponent } from './food-beverage/bars/bars.component';
+import { LifestyleComponent } from './lifestyle/lifestyle.component';
+import { WellnessAndSpaComponent } from './lifestyle/wellness-and-spa/wellness-and-spa.component';
+import { ResortActivitiesComponent } from './lifestyle/resort-activities/resort-activities.component';
+import { ExperiencesComponent } from './lifestyle/experiences/experiences.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccommodationComponent,
+    RoomsComponent,
+    FoodBeverageComponent,
+    RestaurantComponent,
+    BarsComponent,
+    LifestyleComponent,
+    WellnessAndSpaComponent,
+    ResortActivitiesComponent,
+    ExperiencesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
