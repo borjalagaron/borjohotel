fb0611adf9d4f0202c6d1351df50649a81d38b3e
Borja Lagaron <borjo@Borjas-MacBook-Air.local>
2019-05-27 23:46:11 +0200

    Routing done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccommodationComponent } from './accommodation/accommodation.component';
+import { CelebrationsComponent } from './celebrations/celebrations.component';
+import { FoodAndBeverageComponent } from './food-and-beverage/food-and-beverage.component';
+import { LifestyleComponent } from './lifestyle/lifestyle.component';
+import { OffersComponent } from './offers/offers.component';
+import { WeddingsComponent } from './weddings/weddings.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'accomodation', component: AccommodationComponent },
+  { path: 'celebrations', component: CelebrationsComponent },
+  { path: 'food-and-beverage', component: FoodAndBeverageComponent },
+  { path: 'lifestyle', component: LifestyleComponent },
+  { path: 'offers', component: OffersComponent },
+  { path: 'weddings', component: WeddingsComponent },
+  { path: 'signin', component: SigninComponent },
+  { path: 'signup', component: SignupComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { AccommodationComponent } from './accommodation/accommodation.component';
-import { FoodBeverageComponent } from './food-beverage/food-beverage.component';
-import { RestaurantComponent } from './food-beverage/restaurant/restaurant.component';
-import { BarsComponent } from './food-beverage/bars/bars.component';
+import { RoomsComponent } from './accommodation/rooms/rooms.component';
+import { FoodAndBeverageComponent } from './food-and-beverage/food-and-beverage.component';
+import { RestaurantComponent } from './food-and-beverage/restaurant/restaurant.component';
+import { BarsComponent } from './food-and-beverage/bars/bars.component';
 import { LifestyleComponent } from './lifestyle/lifestyle.component';
 import { WellnessAndSpaComponent } from './lifestyle/wellness-and-spa/wellness-and-spa.component';
 import { ResortActivitiesComponent } from './lifestyle/resort-activities/resort-activities.component';
@@ -25,30 +26,30 @@ import { SignupComponent } from './auth/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
     AccommodationComponent,
-    RoomsComponent,
-    FoodBeverageComponent,
-    RestaurantComponent,
-    BarsComponent,
-    LifestyleComponent,
-    WellnessAndSpaComponent,
-    ResortActivitiesComponent,
-    ExperiencesComponent,
-    ExcursionsComponent,
-    WeddingsComponent,
-    ThaiWeddingsComponent,
-    WesternWeddingsComponent,
-    CelebrationsComponent,
-    OffersComponent,
+    AppComponent,
     AuthComponent,
+    BarsComponent,
+    CelebrationsComponent,
+    ExcursionsComponent,
+    ExperiencesComponent,
+    FoodAndBeverageComponent,
+    LifestyleComponent,
+    OffersComponent,
+    ResortActivitiesComponent,
+    RestaurantComponent,
+    RoomsComponent,
     SigninComponent,
-    SignupComponent
+    SignupComponent,
+    ThaiWeddingsComponent,
+    WeddingsComponent,
+    WellnessAndSpaComponent,
+    WesternWeddingsComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    BrowserModule,
     MaterialModule
   ],
   providers: [],
